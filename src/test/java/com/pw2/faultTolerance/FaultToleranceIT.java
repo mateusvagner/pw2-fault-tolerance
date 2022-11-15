c4cdf6d0be19b124b055077591c1664a219f2336
Mateus Vagner <mateus.vagner@gmail.com>
2022-11-15 10:11:16 -0300

    add files

---
+++ src/test/java/com/pw2/faultTolerance/FaultToleranceIT.java
@@ -0,0 +1,8 @@
+package com.pw2.faultTolerance;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class FaultToleranceIT extends FaultToleranceTest {
+    // Execute the same tests but in packaged mode.
+}
